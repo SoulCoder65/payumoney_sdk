9c60bed1395e8021f8ed55b05ff2903ca159fc22
SoulCoder65 <akshaysaxena860@gmail.com>
2021-11-07 20:58:12 +0530

    Remove Add para

--- lib/payumoney_sdk.dart
+++ lib/payumoney_sdk.dart
@@ -37,14 +37,7 @@ class PayumoneySdk {
      String? udf3,
      String? udf4,
      String? udf5,
-     String? udf6,
-     String? udf7,
-     String? udf8,
-     String? udf9,
-     String? udf10,
-
-
-    
+     
    })async{
      ///Success URL="https://www.payumoney.com/mobileapp/payumoney/success.php"
      ///Falure URL="https://www.payumoney.com/mobileapp/payumoney/failure.php"
@@ -71,12 +64,7 @@ class PayumoneySdk {
         "udf3":udf3,
         "udf4":udf4,
         "udf5":udf5,
-        "udf6":udf6,
-        "udf7":udf7,
-        "udf8":udf8,
-        "udf9":udf9,
-        "udf10":udf10,
-     });
+        });
 
 
 
